5a826c5fbdea1cfc81a95d6bd5f62c00ce0c5499
yam4qa <67244217+yam4qa@users.noreply.github.com>
2020-07-23 18:08:13 -0500

    commiting from branch with lil changes

---
+++ src/test/java/testNGPackA/Test.java
@@ -0,0 +1,17 @@
+package testNGPackA;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test 
+{
+
+}
